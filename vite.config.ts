def75f465ff3ecd4baef28a2cf9e2bb94b4ff91c
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2025-12-30 17:54:16 -0800

    update link

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/realhand-website/" : "/",
+  base: "realhand-website",
   server: {
     host: "::",
     port: 8080,
